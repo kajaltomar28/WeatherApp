b633190149e3f36cd2bdf1240a80677cdc5862c6
kajaltomar28 <89395100+kajaltomar28@users.noreply.github.com>
2023-09-28 12:22:13 +0530

    Add files via upload
    This JavaScript file contains the functionality of your web app. It handles user input, makes API requests, and updates the DOM with weather information and error messages.

---
+++ app.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const locationInput = document.getElementById('locationInput');
+    const getWeatherButton = document.getElementById('getWeatherButton');
+    const weatherInfo = document.getElementById('weatherInfo');
+    const unitToggle = document.getElementById('unitToggle');
+
+    getWeatherButton.addEventListener('click', () => {
+        const location = locationInput.value.trim();
+        const unit = unitToggle.value;
+        if (location === '') {
+            showError('Please enter a location.');
+            return;
+        }
+        
+        // You'll need to replace 'YOUR_API_KEY' with an actual API key from a weather service provider.
+        const apiKey = '35112a1179088fdcac9c8e46d06679ac';
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=${apiKey}`;
+
+        fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`API request failed with status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.cod !== 200) {
+                throw new Error(`Weather API error: ${data.message}`);
+            }
+            displayWeather(data);
+        })
+        .catch(error => {
+            showError(`Error: ${error.message}`);
+        });
+
+    });
+
+    function displayWeather(data) {
+        const temperature = data.main.temp;
+        const humidity = data.main.humidity;
+        const windSpeed = data.wind.speed;
+        const weatherDescription = data.weather[0].description;
+
+        const unitLabel = unitToggle.options[unitToggle.selectedIndex].text;
+        
+        weatherInfo.innerHTML = `
+            <h2>Current Weather</h2>
+            <p>Temperature: ${temperature} ${unitLabel}</p>
+            <p>Humidity: ${humidity}%</p>
+            <p>Wind Speed: ${windSpeed} m/s</p>
+            <p>Condition: ${weatherDescription}</p>
+        `;
+    }
+
+    function showError(message) {
+        weatherInfo.innerHTML = `<p class="error">${message}</p>`;
+    }
+});
